8bc854c87420b253f4f4262b22f740dcb5017f9a
ubuntu <ubuntu@ubuntu>
2020-08-05 12:52:25 +0000

    ss

--- startEclipse.sh
+++ startEclipse.sh
@@ -4,9 +4,10 @@ export BASE_PATH="/work/ABATech20"
 
 echo $BASE_PATH
 
+export JAVA_HOME=/work/ABATech20/tools/Java/java-8-openjdk-amd64
 export TOOL_PATH=$BASE_PATH/tools
 export ECLIPSE_HOME=$TOOL_PATH/eclipse
-export GLASSFISH_HOME=$TOOL_PATH/glassfish3
+export GLASSFISH_HOME=$TOOL_PATH/payara
 #export ECLIPSE_WORKSPACE=$BASE_PATH/workspace
 export M2_HOME=$TOOL_PATH/apache-maven
 export M2_REPO=$BASE_PATH/.m2/repository
